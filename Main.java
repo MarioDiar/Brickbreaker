3c0498a41ee5d926b82be199760df852423399f2
MarioDiar <mariodiar@gmail.com>
2014-09-29 19:40:58 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+/**
+ *
+ * @author MarioDiaz
+ */
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main (String[] args){
+        BrickBreaker appBrickbreaker = new BrickBreaker ();
+        appBrickbreaker.setVisible(true);
+        appBrickbreaker.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
